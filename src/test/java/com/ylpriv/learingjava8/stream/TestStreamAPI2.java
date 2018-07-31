036339479c93cf17c44b80a36851ce38bc559be8
Peter <V814429032@gmail.com>
2018-07-31 10:59:03 +0800

    Initial commit

---
+++ src/test/java/com/ylpriv/learingjava8/stream/TestStreamAPI2.java
@@ -0,0 +1,45 @@
+package com.ylpriv.learingjava8.stream;
+
+import com.ylpriv.learingjava8.entiry.Employee;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TestStreamAPI2 {
+
+    //2. 中间操作
+    List<Employee> emps = Arrays.asList(
+            new Employee(102, "李四", 59, 6666.66),
+            new Employee(101, "张三", 18, 9999.99),
+            new Employee(103, "王五", 28, 3333.33),
+            new Employee(104, "赵六", 8, 7777.77),
+            new Employee(104, "赵六", 8, 7777.77),
+            new Employee(104, "赵六", 8, 7777.77),
+            new Employee(105, "田七", 38, 5555.55)
+    );
+	/*
+	  筛选与切片
+		filter——接收 Lambda ， 从流中排除某些元素。
+		    接收一个断言型函数式接口Predicate<? super T> predicate
+		limit——截断流，使其元素不超过给定数量。
+		skip(n) —— 跳过元素，返回一个扔掉了前 n 个元素的流。若流中元素不足 n 个，则返回一个空流。与 limit(n) 互补
+		distinct——筛选，通过流所生成元素的 hashCode() 和 equals() 去除重复元素
+	 */
+
+    //内部迭代：迭代操作 Stream API 内部完成
+    @Test
+    public void test2(){
+        //所有的中间操作不会做任何的处理(Lambda表达式在这一步不会输出)
+        Stream<Employee> stream = emps.stream()
+                .filter((e) -> {
+                    System.out.println("测试中间操作");
+                    return e.getAge() <= 35;
+                });
+
+        //只有当做终止操作时，所有的中间操作会一次性的全部执行，称为“惰性求值”
+        stream.forEach(System.out::println);
+    }
+
+}
